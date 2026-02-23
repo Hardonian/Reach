5a168911ea85582f4e5d8d5eb3d34b8e54f497ea
Scott Hardie <scottrmhardie@gmail.com>
2026-02-23 11:47:47 -0500

    FF

--- apps/arcade/src/app/governance/page.tsx
+++ apps/arcade/src/app/governance/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { PolicyRow } from "@/components/PolicyRow";
 import { EmptyState } from "@/components/EmptyState";
 import { StatusIndicator } from "@/components/StatusIndicator";
