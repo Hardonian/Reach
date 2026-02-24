5064c349c0823e4b8ede09a7ffafdd8ae440ce1f
Scott Hardie <scottrmhardie@gmail.com>
2026-02-23 23:26:04 -0500

    Remove remote Material Symbols link and add dedicated engine CI check

--- apps/arcade/src/app/layout.tsx
+++ apps/arcade/src/app/layout.tsx
@@ -69,9 +69,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
-      </head>
       <body className="min-h-screen flex flex-col font-sans">
         <NavBar />
         <main className="flex-1">{children}</main>
